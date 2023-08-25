c64b07c750231563d417cb1e83a1eb03068e7bbe
Danil Takiylin <danadanil900@gmail.com>
2023-08-25 22:27:35 +0200

    feat: added page markup Favorite

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,6 @@ export * from 'components/App';
 export * from 'components/Layout';
 export * from 'components/shared/Section/Section';
 export * from 'components/shared/Container/Container';
+export * from 'components/Favorite/FavoritePage';
+export * from 'components/RecipesList/RecipesList';
+export * from 'components/RecipesItem/RecipesItem';
