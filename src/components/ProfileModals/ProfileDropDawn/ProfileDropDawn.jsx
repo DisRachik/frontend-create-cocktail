876ea47f96f85e3bceb87a6058f3615175a7c9a6
DeyKast <deykast@gmail.com>
2023-08-26 16:31:22 +0300

    start-work

---
+++ src/components/ProfileModals/ProfileDropDawn/ProfileDropDawn.jsx
@@ -0,0 +1,13 @@
+const ProfileDropDawn = () => {
+  return (
+    <div>
+      <div>
+        <p>Edit profile</p>
+        <svg class="" width="" height="">
+          <use href=""></use>
+        </svg>
+      </div>
+      <button>Log out</button>
+    </div>
+  );
+};
