cb903509847aed42c716dc3455bad2cb5c93aacf
DisRachik <radchenkoig@gmail.com>
2023-08-26 15:05:26 +0300

    feat: add media rules for container

--- src/components/shared/Container/Container.styled.jsx
+++ src/components/shared/Container/Container.styled.jsx
@@ -5,5 +5,13 @@ export const ContainerWrap = styled.section`
 
   max-width: 1440px;
   margin: 0 auto;
-  padding: 0 100px;
+  padding: 0 20px;
+
+  @media (min-width: 768px) {
+    padding: 0 32px;
+  }
+
+  @media (min-width: 1200px) {
+    padding: 0 100px;
+  }
 `;
--- src/components/shared/Section/Section.styled.jsx
+++ src/components/shared/Section/Section.styled.jsx
@@ -7,14 +7,26 @@ export const SectionWrap = styled.section`
   max-width: 1440px;
   min-height: 100%;
   margin: 0 auto;
-  padding: 160px 100px 140px 100px;
+  padding: 80px 20px;
+
+  @media (min-width: 768px) {
+    padding: 140px 32px;
+  }
+
+  @media (min-width: 1200px) {
+    padding: 160px 100px 140px 100px;
+  }
 `;
 
 export const SectionSubtitle = styled.p`
-  margin-top: -28px;
+  margin-top: -22px;
   margin-bottom: 8px;
   opacity: 0.5;
   line-height: 1.25;
+
+  @media (min-width: 768px) {
+    margin-top: -28px;
+  }
 `;
 
 export const SectionTitle = styled.h2`
