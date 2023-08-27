520245dbb418be85eb28d03e27731dfd062a62b6
Serhii-Ivasiuk <ivasiuk.sa@gmail.com>
2023-08-27 18:27:17 +0300

    Update AddRecipeForm.jsx

--- src/components/AddRecipeForm/AddRecipeForm.jsx
+++ src/components/AddRecipeForm/AddRecipeForm.jsx
@@ -41,8 +41,6 @@ export const AddRecipeForm = () => {
     formState: { errors },
   } = useForm(formSettings);
 
-  console.log('render');
-
   const [formState, setFormState] = useState({ ...initialValues });
 
   const categories = useSelector(selectCategories.data);
