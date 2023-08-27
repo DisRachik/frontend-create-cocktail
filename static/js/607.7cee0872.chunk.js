3f2d451b998ae6721bba5718162a8035031a4a55
DisRachik <103518662+DisRachik@users.noreply.github.com>
2023-08-27 11:10:23 +0000

    Deploying to gh-pages from @ DisRachik/frontend-create-cocktail@3316aaf443d79d74ca0a56f744175cc7745cc64b 🚀

--- static/js/607.7cee0872.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkfrontend_create_cocktail=self.webpackChunkfrontend_create_cocktail||[]).push([[607],{5607:function(t,e,n){n.r(e);var r=n(4904),a=n(184);e.default=function(){return(0,a.jsx)(r.$0,{pretitle:"Highball glass",title:"Pornstar Martini"})}}}]);
-//# sourceMappingURL=607.7cee0872.chunk.js.map
--- static/js/865.4cb7429d.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkfrontend_create_cocktail=self.webpackChunkfrontend_create_cocktail||[]).push([[865],{6865:function(e,t,r){r.r(t);var n=r(4904),c=r(184);t.default=function(){return(0,c.jsx)(c.Fragment,{children:(0,c.jsx)(n.$0,{title:"Craft Your Perfect Drink with Drink Master"})})}}}]);
-//# sourceMappingURL=865.4cb7429d.chunk.js.map
