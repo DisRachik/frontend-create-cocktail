7418335a3e4ef3c3855dec2d59e189064e881407
Ruslan <Giovingo92@gmail.com>
2023-08-27 14:52:43 +0300

    feat: add container

--- src/components/Welcome/WelcomePage.jsx
+++ src/components/Welcome/WelcomePage.jsx
@@ -1,10 +1,17 @@
+import { ContainerWrap } from 'components/shared/Container/Container.styled';
 import { NavLink } from 'react-router-dom';
 
 export const WelcomePage = () => {
   return (
-    <>
-      <NavLink to="/signup">Registration</NavLink>
-      <NavLink to="/signin">Sign In</NavLink>
-    </>
+    <ContainerWrap>
+      <ul>
+        <li>
+          <NavLink to="/signup">Registration</NavLink>
+        </li>
+        <li>
+          <NavLink to="/signin">Sign In</NavLink>
+        </li>
+      </ul>
+    </ContainerWrap>
   );
 };
