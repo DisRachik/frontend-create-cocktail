b8ea6ab3fcb1644bcf4b7019014869dd9e281d44
Ruslan <Giovingo92@gmail.com>
2023-08-30 16:04:01 +0300

    feat: add basic styles for signup page

--- src/components/Signup/SignupPage.jsx
+++ src/components/Signup/SignupPage.jsx
@@ -1,13 +1,21 @@
-import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useAuth } from 'redux/auth/useAuth';
-import { ContainerWrap } from 'components/shared/Container/Container.styled';
-import { SectionWrap } from 'components/shared/Section/Section.styled';
-import { BackgroundImage } from 'components/Welcome/WelcomePage.styled';
+import {
+  BackgroundImage,
+  ContainerWelcome,
+  SectionWelcome,
+  WelcomeWrap,
+} from 'components/Welcome/WelcomePage.styled';
 import { Button, FormIcons, FormMessages } from 'components';
 import { Input, InputBox } from 'components/SubscribeForm/SubscribeForm.styled';
-import { FormWrap, LinkWrap } from './SignupPage.styled';
+import {
+  ButtonWrap,
+  FormWrap,
+  Link,
+  LinkWrap,
+  SignUpTitle,
+} from './SignupPage.styled';
 import { signUpSchema } from 'schema/signUpSchema';
 
 export const SignupPage = () => {
@@ -30,78 +38,83 @@ export const SignupPage = () => {
   };
 
   return (
-    <ContainerWrap>
+    <ContainerWelcome>
       <BackgroundImage>
-        <SectionWrap>
-          <FormWrap onSubmit={handleSubmit(onSubmit)}>
-            <InputBox>
-              <Input
-                type="name"
-                name="name"
-                placeholder="Name"
-                {...register('name')}
-                valid={isValid}
-                invalid={isDirty && !isValid}
+        <SectionWelcome>
+          <WelcomeWrap>
+            <SignUpTitle>Registration</SignUpTitle>
+            <FormWrap onSubmit={handleSubmit(onSubmit)}>
+              <InputBox>
+                <Input
+                  type="name"
+                  name="name"
+                  placeholder="Name"
+                  {...register('name')}
+                  valid={isValid}
+                  invalid={isDirty && !isValid}
+                />
+                <FormIcons valid={isValid} invalid={!isValid && isDirty} />
+              </InputBox>
+              <FormMessages
+                invalidValue={errors.name}
+                validValue={isValid && isDirty}
+                errorMessage={errors.name?.message}
+                checkMessage="This is valid name"
               />
-              <FormIcons valid={isValid} invalid={!isValid && isDirty} />
-            </InputBox>
-            <FormMessages
-              invalidValue={errors.name}
-              validValue={isValid && isDirty}
-              errorMessage={errors.name?.message}
-              checkMessage="This is valid name"
-            />
 
-            <InputBox>
-              <Input
-                type="email"
-                name="email"
-                placeholder="Email"
-                {...register('email')}
-                valid={isValid}
-                invalid={isDirty && !isValid}
+              <InputBox>
+                <Input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  {...register('email')}
+                  valid={isValid}
+                  invalid={isDirty && !isValid}
+                />
+                <FormIcons valid={isValid} invalid={!isValid && isDirty} />
+              </InputBox>
+              <FormMessages
+                invalidValue={errors.email}
+                validValue={isValid && isDirty}
+                errorMessage={errors.name?.message}
+                checkMessage="This is valid email"
               />
-              <FormIcons valid={isValid} invalid={!isValid && isDirty} />
-            </InputBox>
-            <FormMessages
-              invalidValue={errors.email}
-              validValue={isValid && isDirty}
-              errorMessage={errors.name?.message}
-              checkMessage="This is valid email"
-            />
 
-            <InputBox>
-              <Input
-                type="password"
-                name="password"
-                placeholder="Password"
-                {...register('password')}
-                valid={isValid}
-                invalid={isDirty && !isValid}
+              <InputBox>
+                <Input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  {...register('password')}
+                  valid={isValid}
+                  invalid={isDirty && !isValid}
+                />
+                <FormIcons valid={isValid} invalid={!isValid && isDirty} />
+              </InputBox>
+              <FormMessages
+                invalidValue={errors.password}
+                validValue={isValid && isDirty}
+                errorMessage={errors.password?.message}
+                checkMessage="This is valid password"
               />
-              <FormIcons valid={isValid} invalid={!isValid && isDirty} />
-            </InputBox>
-            <FormMessages
-              invalidValue={errors.password}
-              validValue={isValid && isDirty}
-              errorMessage={errors.password?.message}
-              checkMessage="This is valid password"
-            />
 
-            <Button
-              disabled={!isValid || !isDirty}
-              transparent
-              minWidth={'100%'}
-              minHeight="56px"
-            >
-              Subscribe
-            </Button>
-            <LinkWrap>
-              <Link to="/signin">Sign In</Link>
-            </LinkWrap>
-          </FormWrap>
-        </SectionWrap>
+              <ButtonWrap>
+                <Button
+                  disabled={!isValid || !isDirty}
+                  transparent
+                  minWidth={'100%'}
+                  minHeight="56px"
+                >
+                  Sign Up
+                </Button>
+              </ButtonWrap>
+              <LinkWrap>
+                <Link to="/signin">Sign In</Link>
+              </LinkWrap>
+            </FormWrap>
+          </WelcomeWrap>
+        </SectionWelcome>
       </BackgroundImage>
-    </ContainerWrap>
+    </ContainerWelcome>
   );
 };
--- src/components/Signup/SignupPage.styled.jsx
+++ src/components/Signup/SignupPage.styled.jsx
@@ -5,10 +5,84 @@ export const FormWrap = styled.form`
   max-width: 400px;
 `;
 
-export const Link = styled(NavLink)``;
+export const SignUpTitle = styled.h2`
+  color: #f3f3f3;
+  font-feature-settings: 'liga' off;
+  font-family: Manrope;
+  font-size: 28px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 32px; /* 114.286% */
+  letter-spacing: -0.56px;
+
+  text-align: start;
+  margin-bottom: 28px;
+
+  @media (min-width: 768px) {
+    color: #f3f3f3;
+    font-feature-settings: 'liga' off;
+    font-family: Manrope;
+    font-size: 40px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 44px; /* 110% */
+    letter-spacing: -0.8px;
+  }
+
+  @media (min-width: 1200px) {
+    color: #f3f3f3;
+    font-feature-settings: 'liga' off;
+    font-family: Manrope;
+    font-size: 40px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 44px; /* 110% */
+    letter-spacing: -0.8px;
+  }
+`;
+
+export const ButtonWrap = styled.div`
+  margin-top: 28px;
+
+  @media (min-width: 768px) {
+    margin-top: 40px;
+  }
+
+  /* @media (min-width: 1200px) {
+  } */
+`;
 
 export const LinkWrap = styled.div`
   margin-top: 14px;
-
   text-align: center;
+`;
+
+export const Link = styled(NavLink)`
+  color: #f3f3f3;
+  font-family: Manrope;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 16px; /* 133.333% */
+  text-decoration-line: underline;
+
+  @media (min-width: 768px) {
+    color: #f3f3f3;
+    font-family: Manrope;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 18px; /* 128.571% */
+    text-decoration-line: underline;
+  }
+
+  @media (min-width: 1200px) {
+    color: #f3f3f3;
+    font-family: Manrope;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 18px; /* 128.571% */
+    text-decoration-line: underline;
+  }
 `;
