ad2a706d68319574f00f3b75730f84d28496feee
Danil Takiylin <danadanil900@gmail.com>
2023-08-31 21:59:55 +0200

    refactor: refactor btnLoadMore

--- src/api/favorite.js
+++ src/api/favorite.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const getFavorites = async (page = 1, limit = 3) => {
-  const res = await axios.get(`/favorites?page=${page}&limit=${limit}`);
+  const res = await axios.get(`/favorite?page=${page}&limit=${limit}`);
 
   return res.data;
 };
--- src/components/Favorite/FavoritePage.jsx
+++ src/components/Favorite/FavoritePage.jsx
@@ -1,30 +1,47 @@
 import { EmptyFavoritePage, RecipesList } from 'components';
 import { useDispatch, useSelector } from 'react-redux';
-import { getFavoriteDrinks } from 'redux/favorite/selectors';
-import { fetchUserFavoriteDrinks } from 'redux/favorite/operations';
+import {
+  getFavoriteDrinks,
+  getTotalHitsFavorite,
+} from 'redux/favorite/selectors';
+
+import {
+  changeFavoriteStatus,
+  fetchUserFavoriteDrinks,
+} from 'redux/favorite/operations';
 import { useEffect, useState } from 'react';
 import { ButtonLoadMore } from 'components/ButtonLoadMore/ButtonLoadMore';
 
 export const FavoritePage = () => {
   const [page, setPage] = useState(1);
+
   const dispatch = useDispatch();
+
   const { favoriteDrinks } = useSelector(getFavoriteDrinks);
 
+  const TotalHitsFavorite = useSelector(getTotalHitsFavorite);
+
   useEffect(() => {
-    dispatch(fetchUserFavoriteDrinks({ page, limit: 2 }));
+    dispatch(fetchUserFavoriteDrinks({ page, limit: 9 }));
   }, [dispatch, page]);
 
   const seeMoreDrinks = () => {
     setPage(prevState => prevState + 1);
-    dispatch(fetchUserFavoriteDrinks({ page, limit: 2 }));
+    dispatch(fetchUserFavoriteDrinks({ page, limit: 9 }));
+  };
+
+  const handleClick = async id => {
+    dispatch(changeFavoriteStatus(id));
   };
 
   return (
     <>
       {favoriteDrinks.length !== 0 ? (
         <>
-          <RecipesList array={favoriteDrinks} />
-          <ButtonLoadMore onClick={seeMoreDrinks} />
+          <RecipesList array={favoriteDrinks} action={handleClick} />
+          {TotalHitsFavorite > favoriteDrinks.length && (
+            <ButtonLoadMore onClick={seeMoreDrinks} />
+          )}
         </>
       ) : (
         <EmptyFavoritePage />
