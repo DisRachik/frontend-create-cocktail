54876fc77307ad4ffa2196f8e904901fe2bd4e1b
Yuliya1913 <Lera-855@email.ua>
2023-08-31 22:10:17 +0300

    add delete batton

--- src/api/myRecipes/deleteMyRecip.js
+++ src/api/myRecipes/deleteMyRecip.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-export const deleteMyRecip = async id => {
-  const res = await axios.delete(`/own/${id}`);
+export const deleteMyRecip = async _id => {
+  const res = await axios.delete(`/own/${_id}`);
 
   return res.data;
 };
