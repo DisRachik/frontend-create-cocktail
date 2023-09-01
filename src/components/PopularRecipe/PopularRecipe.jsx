e9d8e33d6ff24c13b08c3a7de5285ec1bcbb8161
Serhii-Ivasiuk <ivasiuk.sa@gmail.com>
2023-09-02 02:32:25 +0300

    feat: add popular recipes (without styled for now - will add it ASAP)

--- src/components/PopularRecipe/PopularRecipe.jsx
+++ src/components/PopularRecipe/PopularRecipe.jsx
@@ -1,3 +1,38 @@
+// Libs
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+// Api
+import { getPopularRecipes } from 'api';
+
 export const PopularRecipe = () => {
-  return <div style={{ color: 'red' }}>PopularRecipe:</div>;
+  const [popularRecipes, setPopularRecipes] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const popular = await getPopularRecipes();
+      setPopularRecipes(popular);
+    })();
+  }, []);
+
+  return (
+    <ul>
+      {popularRecipes.map(({ _id, drinkThumb, drink, about }) => {
+        return (
+          <li>
+            <Link to={`/recipe/${_id}`}>
+              <img src={drinkThumb} alt={drink} width="90" height="90" />
+              <div>
+                <h3>{drink}</h3>
+                <p>
+                  {about
+                    ? about
+                    : 'We are sorry, but there is no description for this cocktail yet.'}
+                </p>
+              </div>
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
