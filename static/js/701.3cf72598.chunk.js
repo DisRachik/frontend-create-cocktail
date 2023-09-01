6271e96e9375b71c751a08177501643e26a985c9
AndreyTarkaniy <114865777+AndreyTarkaniy@users.noreply.github.com>
2023-09-01 15:13:34 +0000

    Deploying to gh-pages from @ DisRachik/frontend-create-cocktail@f527495c0c32d7cd8559b90271c4651f31a76dc5 🚀

--- static/js/701.3cf72598.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkfrontend_create_cocktail=self.webpackChunkfrontend_create_cocktail||[]).push([[701],{701:function(e,t,c){c.r(t);var n=c(3023),r=c(184);t.default=function(){return(0,r.jsx)(n.Gg,{})}}}]);
-//# sourceMappingURL=701.3cf72598.chunk.js.map
