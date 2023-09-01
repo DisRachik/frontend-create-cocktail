f62746ee8bb2bdb9b5ee538ccdcacd66d359c453
AndreyTarkaniy <endrubass@gmail.com>
2023-09-01 17:15:08 +0300

    fixx prev request

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -5,30 +5,32 @@ import { Li, MenuLink, NavList } from './Navigation.styled';
 export const Navigation = ({ nav }) => {
   //   const [close, setClose] = useState(false);
   return (
-    <NavList nav={nav}>
-      <Li>
-        <MenuLink
-          to="/main"
-          //   onClick={() => {
-          //     setClose(!close);
-          //   }}
-        >
-          Home
-        </MenuLink>
-      </Li>
-      <Li>
-        <MenuLink to="/drinks">Drinks</MenuLink>
-      </Li>
-      <Li>
-        <MenuLink to="/add">Add recipe</MenuLink>
-      </Li>
-      <Li>
-        <MenuLink to="/my">My recipe</MenuLink>
-      </Li>
-      <Li>
-        <MenuLink to="/favorite">Favorite</MenuLink>
-      </Li>
-    </NavList>
+    <>
+      <NavList nav={nav}>
+        <Li>
+          <MenuLink
+            to="/main"
+            //   onClick={() => {
+            //     setClose(!close);
+            //   }}
+          >
+            Home
+          </MenuLink>
+        </Li>
+        <Li>
+          <MenuLink to="/drinks">Drinks</MenuLink>
+        </Li>
+        <Li>
+          <MenuLink to="/add">Add recipe</MenuLink>
+        </Li>
+        <Li>
+          <MenuLink to="/my">My recipe</MenuLink>
+        </Li>
+        <Li>
+          <MenuLink to="/favorite">Favorite</MenuLink>
+        </Li>
+      </NavList>
+    </>
   );
 };
 Navigation.propTypes = {
