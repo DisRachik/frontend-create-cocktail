1aaa48b854e2d563a76ffe5240e170ee7cee6443
Vladimir Minakov <Vladimirmvk@gmail.com>
2023-09-03 14:13:05 +0300

    Адаптация

--- src/components/Main/MainSection.styled.jsx
+++ src/components/Main/MainSection.styled.jsx
@@ -5,19 +5,22 @@ import background2 from "../../img/mae-mu-T7heq8rawkc-unsplash 2.png";
 import background3 from "../../img/mae-mu-T7heq8rawkc-unsplash 3.png";
 
 export const MainSectionWrap = styled.section`
-  //outline: 1px solid tomato;
- display: flex-inline;
- justify-content: st;
- flex-direction: column;
+// outline: 1px solid tomato;
 
-  // gap: 28px;
   max-width: 1440px;
-  min-height: 100%;
+  height: 796px;
   margin: 0 auto;
-   padding: 80px 20px;
+  // padding: 80px 20px;
+
+  @media screen and (min-width: 375px) {
+    height: 1001px;
+    margin: 0 auto;
+    }
+
 
   @media (min-width: 768px) {
      padding: 140px 32px;
+     height: 790px;
 //      width: 500px;
 //   height: 72px;
 //   margin-left: 100px;
@@ -28,7 +31,8 @@ export const MainSectionWrap = styled.section`
 
   @media (min-width: 1200px) {
      padding: 160px 100px 140px 100px;
-//     width: 1440px;
+     max-width: 1440px;
+     height: 790px;
 //   height: 100%; 
 //   margin-left: 100px;
 //   padding-bottom: 316px;
@@ -40,9 +44,37 @@ export const MainSectionWrap = styled.section`
 
 export const MainSectionSubtitle = styled.p`
    
-  margin-bottom: 8px;
+  margin-bottom: 32px;
   color:${theme.colors.lightGray};
   line-height: 1.25;
+  max-width: 266px;
+  font-size: 14px;
+
+
+  @media screen and (min-width: 375px) {
+       
+  margin-bottom: 40px;
+    font-size: ${theme.fontSizes.sectionTitleTablet};
+    max-width: 449px;
+    line-height: 1.07;
+    font-size: 18px;
+  }
+  @media screen and (min-width: 768px) {
+       
+    margin-bottom: 40px;
+    font-size: ${theme.fontSizes.sectionTitleTablet};
+    max-width: 500px;
+    font-size: 18px;
+    line-height: 1.07;
+  }
+  @media screen and (min-width: 1440px) {
+       
+  margin-bottom: 32px;
+    font-size: ${theme.fontSizes.sectionTitle};
+    font-size: 18px;
+    line-height: 1.06;
+  }
+
 
   @media (min-width: 768px) {
    width: 500px;
@@ -56,37 +88,56 @@ line-height: 24px;
 `;
 
 export const MainSectionTitle = styled.h1`
+
+max-width: 279px;
+margin-bottom: 28px;
+font-size: ${theme.fontSizes.sectionTitlePhone};
+color:${theme.colors.lightGray};
+font-weight: 600;
+line-height: 1.18;
+
+@media screen and (min-width: 375px) {
+  font-size: ${theme.fontSizes.sectionTitleTablet};
+  max-width: 483px;
+  line-height: 1.07;
+}
+@media screen and (min-width: 768px) {
+  font-size: ${theme.fontSizes.sectionTitleTablet};
   max-width: 715px;
-  // margin-top: 244px;
-  // color:${theme.colors.lightGray};
-  color:${theme.colors.lightGray};
-//  margin-left: 100px;
- margin-bottom: 28px;
+  line-height: 1.07;
+}
+@media screen and (min-width: 1440px) {
   font-size: ${theme.fontSizes.sectionTitle};
-  font-weight: 600;
-  line-height: 1.0625;
+
+  line-height: 1.06;
+}
 `;
 export const BackgroundImageWrap = styled.div`
+
+max-width: 375px;
+    height: 700px; 
 background-size: 496px 700px;
   background-repeat: no-repeat;
- background-position: top 106 left 102;
+ background-position: top 106px left 102px;
 background-image: linear-gradient(0.95deg, #0A0A11 -0.56%, rgba(10, 10, 17, 0) 21.93%),
  linear-gradient(86.16deg, #0A0A11 8.41%, rgba(10, 10, 17, 0) 46.6%),
-    url('${background3}');
+    url('${background1}');
 
-   @media screen and (min-width: 480px) {
-// width: 769px;
-//   height: 100%; 
+
+   @media screen and (min-width: 375px) {
+    max-width: 769px;
+    height: 1001px; 
   background-size: 710px 1001px; 
+  background-position: top 0 left 323px;
 background-image: linear-gradient(0.95deg, #0A0A11 -0.56%, rgba(10, 10, 17, 0) 21.93%),
  linear-gradient(86.16deg, #0A0A11 8.41%, rgba(10, 10, 17, 0) 46.6%),
-    url('${background2}');
+    url('${background1}');
 
    }
 
     @media screen and (min-width: 768px) {   
-    width: 1440px;
-  height: 790px; 
+    max-width: 1440px;
+   height: 790px; 
     margin: 0 auto;
   background-size: 710px 790px;
   background-repeat: no-repeat;
