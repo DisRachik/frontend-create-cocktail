c91d96a3423d4ebc70e0cb43b47ace338687db7d
Natali <114168847+Natali9108@users.noreply.github.com>
2023-09-04 00:15:39 +0300

    fix: hover

--- src/schema/nameSchema.js
+++ src/schema/nameSchema.js
@@ -3,8 +3,6 @@ import * as yup from 'yup';
 export const nameSchema = yup
   .object()
   .shape({
-    name: yup
-      .string()
-      .min(2, 'The name must consist of at least two characters'),
+    name: yup.string().min(2),
   })
   .required();
