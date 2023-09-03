62a15cb1a5e10abd048a13e3fde21b40a3b6527d
DisRachik <103518662+DisRachik@users.noreply.github.com>
2023-09-03 11:01:17 +0000

    Deploying to gh-pages from @ DisRachik/frontend-create-cocktail@aecc21e303ad19dc76a94c6c7cdd952977ea51ab 🚀

--- static/js/501.20b870e2.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkfrontend_create_cocktail=self.webpackChunkfrontend_create_cocktail||[]).push([[501],{4501:function(e,t,c){c.r(t);var n=c(4040),r=c(184);t.default=function(){return(0,r.jsx)(n.mW,{})}}}]);
-//# sourceMappingURL=501.20b870e2.chunk.js.map
