6de5d4c90ff61d92582db503b652d6de8a5b8a49
AndreyTarkaniy <endrubass@gmail.com>
2023-09-04 16:16:55 +0300

    add clip-path:polygon() at MotivationModal

--- src/components/MotivationModal/MotivationModal.styled.jsx
+++ src/components/MotivationModal/MotivationModal.styled.jsx
@@ -35,11 +35,37 @@ export const ContentBox = styled.div`
   padding-top: 32px;
   padding-bottom: 14px;
 
-  border-radius: 19px 20% 30% 34%;
+  /* border-radius: 19px 20% 30% 34%; */
 
   backdrop-filter: blur(5px);
   background-image: url('${contentSvg}');
   /* outline: 1px solid tomato; */
+
+  clip-path: polygon(
+    5.55% 0.29%,
+    91.76% 4.26%,
+    97.01% 18px,
+    99.69% 16.25%,
+    100.28% 74.58%,
+    98.06% 79.02%,
+    94.36% 84.09%,
+    86.94% 85.13%,
+    70.67% 85.12%,
+    65.55% 88.18%,
+    60.37% 95.06%,
+    56.07% 99.03%,
+    50.56% 100.7%,
+    43.22% 98.82%,
+    38.94% 93.81%,
+    34.77% 88.88%,
+    30.38% 86.97%,
+    25.6% 86.52%,
+    17.32% 86.73%,
+    10.68% 85.7%,
+    4.6% 82.98%,
+    0.37% 76.03%,
+    -0.32% 6.74%
+  );
 `;
 
 export const ContentText = styled.p`
