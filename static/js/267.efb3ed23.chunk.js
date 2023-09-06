864ee20d746ccf255be43bc4ae68d758e4bb4484
Hruchman-Ruslan <113464276+Hruchman-Ruslan@users.noreply.github.com>
2023-09-06 15:59:04 +0000

    Deploying to gh-pages from @ DisRachik/frontend-create-cocktail@33c2f827c61abd25e888c8b31b6063daa466bc3b 🚀

---
+++ static/js/267.efb3ed23.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfrontend_create_cocktail=self.webpackChunkfrontend_create_cocktail||[]).push([[267],{6267:function(e,n,t){t.r(n);var c=t(5998),r=t(184);n.default=function(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsx)(c.I0,{})})}}}]);
+//# sourceMappingURL=267.efb3ed23.chunk.js.map
