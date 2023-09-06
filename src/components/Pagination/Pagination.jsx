5fabec75e09e21142e09fe71902adc36baaee516
Andrii <sushkoandy1@gmail.com>
2023-09-06 11:36:37 +0300

    feat: clear old version

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -22,38 +22,31 @@ export const Pagination = ({
   }
 
   const visiblePages = [];
-  const maxVisiblePages = 8; // Максимальна кількість видимих сторінок
-  const pageRange = 4; // Кількість сторінок в групі перед "..."
+  const maxVisiblePages = 8;
+  const pageRange = 4;
 
-  // Визначаємо початковий і кінцевий індекси групи сторінок
   let startIndex = Math.max(currentPage - pageRange, 1);
   let endIndex = Math.min(startIndex + maxVisiblePages - 1, totalPages);
 
-  // Визначаємо чи потрібно додавати "..."
   const needEllipsisStart = startIndex > 1;
   const needEllipsisEnd = endIndex < totalPages;
 
-  // Додаємо "<<" (перша сторінка)
   if (startIndex > 1) {
     visiblePages.push(1);
   }
 
-  // Додаємо "..." перед групою
   if (needEllipsisStart) {
     visiblePages.push('...');
   }
 
-  // Додаємо видимі сторінки
   for (let i = startIndex; i <= endIndex; i++) {
     visiblePages.push(i);
   }
 
-  // Додаємо "..." після групи
   if (needEllipsisEnd) {
     visiblePages.push('...');
   }
 
-  // Додаємо ">>" (остання сторінка)
   if (endIndex < totalPages) {
     visiblePages.push(totalPages);
   }
@@ -89,45 +82,6 @@ export const Pagination = ({
     </PaginWrapper>
   );
 };
-// export const Pagination = ({
-//   paginate,
-//   prevPage,
-//   nextPage,
-//   currentPage,
-//   totalPages,
-// }) => {
-//   const pageNumbers = [];
-//   for (let i = 1; i <= totalPages; i++) {
-//     pageNumbers.push(i);
-//   }
-
-//   return (
-//     <PaginWrapper>
-//       {pageNumbers.length > 1 && (
-//         <>
-//           <ArrowBtn onClick={prevPage} disabled={currentPage === 1}>
-//             <ArrowPrev />
-//           </ArrowBtn>
-//           <PaginationList>
-//             {pageNumbers.map(number => (
-//               <PaginListItem key={number}>
-//                 <BtnItem
-//                   className={number === currentPage ? 'active' : ''}
-//                   onClick={() => paginate(number)}
-//                 >
-//                   {number}
-//                 </BtnItem>
-//               </PaginListItem>
-//             ))}
-//           </PaginationList>
-//           <ArrowBtn onClick={nextPage} disabled={currentPage === totalPages}>
-//             <ArrowNext />
-//           </ArrowBtn>
-//         </>
-//       )}
-//     </PaginWrapper>
-//   );
-// };
 
 Pagination.propTypes = {
   paginate: PropTypes.func.isRequired,
